5dad9ce70867c05ad3e3c722d569c992c9289008
Will Tebbutt <wt0881@my.bristol.ac.uk>
2019-02-12 14:30:49 +0000

    Disable all irrelevant tests

--- test/gradcheck.jl
+++ test/gradcheck.jl
@@ -27,108 +27,108 @@ gradtest(f, dims...) = gradtest(f, rand.(Float64, dims)...)
 
 Random.seed!(0)
 
-@test gradient(//, 2, 3) === (1//3, -2//9)
+# @test gradient(//, 2, 3) === (1//3, -2//9)
 
-@test gradtest((x, W, b) -> σ.(W*x .+ b), 5, (2,5), 2)
-@test gradtest((x, W, b) -> σ.(W*x .+ b), (5,3), (2,5), 2)
-@test gradtest((x, W, b) -> logσ.(W*x .+ b), 5, (2,5), 2)
-@test gradtest((x, W, b) -> logσ.(W*x .+ b), (5,3), (2,5), 2)
+# @test gradtest((x, W, b) -> σ.(W*x .+ b), 5, (2,5), 2)
+# @test gradtest((x, W, b) -> σ.(W*x .+ b), (5,3), (2,5), 2)
+# @test gradtest((x, W, b) -> logσ.(W*x .+ b), 5, (2,5), 2)
+# @test gradtest((x, W, b) -> logσ.(W*x .+ b), (5,3), (2,5), 2)
 
-@test gradtest((w, x) -> w'*x, randn(10, 2), randn(10))
-@test gradtest((w, x) -> transpose(w)*x, randn(5,5), randn(5,5))
+# @test gradtest((w, x) -> w'*x, randn(10, 2), randn(10))
+# @test gradtest((w, x) -> transpose(w)*x, randn(5,5), randn(5,5))
 
-@test gradtest(x -> sum(x, dims = (2, 3)), (3,4,5))
-@test gradtest(x -> prod(x, dims = (2, 3)), (3,4,5))
-@test gradtest(x -> prod(x), (3,4,5))
+# @test gradtest(x -> sum(x, dims = (2, 3)), (3,4,5))
+# @test gradtest(x -> prod(x, dims = (2, 3)), (3,4,5))
+# @test gradtest(x -> prod(x), (3,4,5))
 
-@test gradtest(x -> softmax(x).*(1:3), 3)
-@test gradtest(x -> softmax(x).*(1:3), (3,5))
-@test gradtest(x -> logsoftmax(x).*(1:3), 3)
-@test gradtest(x -> logsoftmax(x).*(1:3), (3,5))
+# @test gradtest(x -> softmax(x).*(1:3), 3)
+# @test gradtest(x -> softmax(x).*(1:3), (3,5))
+# @test gradtest(x -> logsoftmax(x).*(1:3), 3)
+# @test gradtest(x -> logsoftmax(x).*(1:3), (3,5))
 
-@test gradtest(conv, rand(10, 3, 2), randn(Float64,2, 3, 2))
-@test gradtest(conv, rand(10, 10, 3, 2), randn(Float64,2, 2, 3, 2))
-@test gradtest(conv, rand(10, 10, 10, 3, 2), randn(Float64,2, 2, 2, 3, 2))
+# @test gradtest(conv, rand(10, 3, 2), randn(Float64,2, 3, 2))
+# @test gradtest(conv, rand(10, 10, 3, 2), randn(Float64,2, 2, 3, 2))
+# @test gradtest(conv, rand(10, 10, 10, 3, 2), randn(Float64,2, 2, 2, 3, 2))
 
-@test gradtest(x -> maxpool(x, (2,2)), rand(10, 10, 3, 2))
-@test gradtest(x -> maxpool(x, (2,2,2)), rand(10, 10, 10, 3, 2))
+# @test gradtest(x -> maxpool(x, (2,2)), rand(10, 10, 3, 2))
+# @test gradtest(x -> maxpool(x, (2,2,2)), rand(10, 10, 10, 3, 2))
 
-@test gradtest(x -> meanpool(x, (2,2)), rand(10, 10, 3, 2))
-@test gradtest(x -> meanpool(x, (2,2,2)), rand(5, 5, 5, 3, 2))
+# @test gradtest(x -> meanpool(x, (2,2)), rand(10, 10, 3, 2))
+# @test gradtest(x -> meanpool(x, (2,2,2)), rand(5, 5, 5, 3, 2))
 
-@test gradtest(x -> permutedims(x, [3,1,2]), rand(4,5,6))
+# @test gradtest(x -> permutedims(x, [3,1,2]), rand(4,5,6))
 
-@test gradtest(x -> repeat(x; inner=2), rand(5))
-@test gradtest(x -> repeat(x; inner=2, outer=3), rand(5))
-@test gradtest(x -> repeat(x; inner=(2,2,1), outer=(1,1,3)), rand(5,4,3))
+# @test gradtest(x -> repeat(x; inner=2), rand(5))
+# @test gradtest(x -> repeat(x; inner=2, outer=3), rand(5))
+# @test gradtest(x -> repeat(x; inner=(2,2,1), outer=(1,1,3)), rand(5,4,3))
 
-@test gradtest(kron, rand(5), rand(3))
-@test gradtest(kron, rand(5), rand(3), rand(8))
-@test gradtest(kron, rand(5,1), rand(3,1))
-@test gradtest(kron, rand(5,1), rand(3,1), rand(8,1))
-@test gradtest(kron, rand(5,2), rand(3,2), rand(8,2))
+# @test gradtest(kron, rand(5), rand(3))
+# @test gradtest(kron, rand(5), rand(3), rand(8))
+# @test gradtest(kron, rand(5,1), rand(3,1))
+# @test gradtest(kron, rand(5,1), rand(3,1), rand(8,1))
+# @test gradtest(kron, rand(5,2), rand(3,2), rand(8,2))
 
-@testset "maximum" begin
-  @test gradtest(maximum, rand(2, 3))
+# @testset "maximum" begin
+#   @test gradtest(maximum, rand(2, 3))
 
-  @test gradtest(x -> maximum(x, dims=1), rand(2, 3))
-  @test gradtest(x -> maximum(x, dims=2), rand(2, 3))
-  @test gradtest(x -> maximum(x, dims=3), rand(2, 3, 4))
+#   @test gradtest(x -> maximum(x, dims=1), rand(2, 3))
+#   @test gradtest(x -> maximum(x, dims=2), rand(2, 3))
+#   @test gradtest(x -> maximum(x, dims=3), rand(2, 3, 4))
 
-  @test gradtest(x -> maximum(x, dims=[1, 2]), rand(2, 3, 4))
-end
+#   @test gradtest(x -> maximum(x, dims=[1, 2]), rand(2, 3, 4))
+# end
 
-@testset "minimum" begin
-  @test gradtest(minimum, rand(2, 3))
+# @testset "minimum" begin
+#   @test gradtest(minimum, rand(2, 3))
 
-  @test gradtest(x -> minimum(x, dims=1), rand(2, 3))
-  @test gradtest(x -> minimum(x, dims=2), rand(2, 3))
-  @test gradtest(x -> minimum(x, dims=3), rand(2, 3, 4))
+#   @test gradtest(x -> minimum(x, dims=1), rand(2, 3))
+#   @test gradtest(x -> minimum(x, dims=2), rand(2, 3))
+#   @test gradtest(x -> minimum(x, dims=3), rand(2, 3, 4))
 
-  @test gradtest(x -> minimum(x, dims=[1, 2]), rand(2, 3, 4))
-end
+#   @test gradtest(x -> minimum(x, dims=[1, 2]), rand(2, 3, 4))
+# end
 
-@testset "backsolve" begin
-  rng, P, Q = MersenneTwister(123456), 10, 9
-  X, Y, y = randn(rng, P, P), randn(rng, P, Q), randn(rng, P)
+# @testset "backsolve" begin
+#   rng, P, Q = MersenneTwister(123456), 10, 9
+#   X, Y, y = randn(rng, P, P), randn(rng, P, Q), randn(rng, P)
 
-  # \
-  @test gradtest(X -> X \ Y, X)
-  @test gradtest(Y -> X \ Y, Y)
-  @test gradtest(X -> X \ y, X)
-  @test gradtest(y -> X \ y, y)
+#   # \
+#   @test gradtest(X -> X \ Y, X)
+#   @test gradtest(Y -> X \ Y, Y)
+#   @test gradtest(X -> X \ y, X)
+#   @test gradtest(y -> X \ y, y)
 
-  # /
-  @test gradtest(X -> Y' / X, X)
-  @test gradtest(Y -> Y' / X, Y)
-  @test gradtest(X -> y' / X, X)
-  @test gradtest(y -> y' / X, y)
-end
+#   # /
+#   @test gradtest(X -> Y' / X, X)
+#   @test gradtest(Y -> Y' / X, Y)
+#   @test gradtest(X -> y' / X, X)
+#   @test gradtest(y -> y' / X, y)
+# end
 
-@testset "Symmetric" begin
-  rng, P = MersenneTwister(123456), 7
-  A = randn(rng, P, P)
-  @test gradtest(Symmetric, A)
-end
+# @testset "Symmetric" begin
+#   rng, P = MersenneTwister(123456), 7
+#   A = randn(rng, P, P)
+#   @test gradtest(Symmetric, A)
+# end
 
-@testset "diag" begin
-  rng, P = MersenneTwister(123456), 10
-  A = randn(rng, P, P)
-  @test gradtest(diag, A)
-end
+# @testset "diag" begin
+#   rng, P = MersenneTwister(123456), 10
+#   A = randn(rng, P, P)
+#   @test gradtest(diag, A)
+# end
 
-@testset "dense + UniformScaling" begin
-  rng = MersenneTwister(123456)
-  A, λ = randn(rng, 10, 10), randn(rng)
-  @test gradtest(A->A + 5I, A)
-  @test gradtest(λ->A + λ[1] * I, [λ])
-end
+# @testset "dense + UniformScaling" begin
+#   rng = MersenneTwister(123456)
+#   A, λ = randn(rng, 10, 10), randn(rng)
+#   @test gradtest(A->A + 5I, A)
+#   @test gradtest(λ->A + λ[1] * I, [λ])
+# end
 
-@testset "cholesky" begin
-  rng, N = MersenneTwister(123456), 5
-  A = randn(rng, N, N)
-  @test gradtest(A->logdet(cholesky(A' * A + 1e-6I)), A)
-end
+# @testset "cholesky" begin
+#   rng, N = MersenneTwister(123456), 5
+#   A = randn(rng, N, N)
+#   @test gradtest(A->logdet(cholesky(A' * A + 1e-6I)), A)
+# end
 
 @testset "Cholesky (getproperty)" begin
   rng, N = MersenneTwister(123456), 5
@@ -149,94 +149,94 @@ end
   @test gradtest(A->Cholesky(A, :L, 0).L, A)
 end
 
-using Distances
+# using Distances
 
-Zygote.refresh()
+# Zygote.refresh()
 
-@testset "distances" begin
-  rng, P, Q, D = MersenneTwister(123456), 10, 9, 8
+# @testset "distances" begin
+#   rng, P, Q, D = MersenneTwister(123456), 10, 9, 8
 
-   # Check sqeuclidean.
-  let
-    x, y = randn(rng, D), randn(rng, D)
-    @test gradtest(x->sqeuclidean(x, y), x)
-    @test gradtest(y->sqeuclidean(x, y), y)
-  end
+#    # Check sqeuclidean.
+#   let
+#     x, y = randn(rng, D), randn(rng, D)
+#     @test gradtest(x->sqeuclidean(x, y), x)
+#     @test gradtest(y->sqeuclidean(x, y), y)
+#   end
 
-   # Check binary colwise.
-  let
-    X, Y = randn(rng, D, P), randn(rng, D, P)
-    @test gradtest(X->colwise(SqEuclidean(), X, Y), X)
-    @test gradtest(Y->colwise(SqEuclidean(), X, Y), Y)
-  end
+#    # Check binary colwise.
+#   let
+#     X, Y = randn(rng, D, P), randn(rng, D, P)
+#     @test gradtest(X->colwise(SqEuclidean(), X, Y), X)
+#     @test gradtest(Y->colwise(SqEuclidean(), X, Y), Y)
+#   end
 
-   # Check binary pairwise.
-  let
-    X, Y = randn(rng, D, P), randn(rng, D, Q)
-    @test gradtest(X->pairwise(SqEuclidean(), X, Y), X)
-    @test gradtest(Y->pairwise(SqEuclidean(), X, Y), Y)
-  end
+#    # Check binary pairwise.
+#   let
+#     X, Y = randn(rng, D, P), randn(rng, D, Q)
+#     @test gradtest(X->pairwise(SqEuclidean(), X, Y), X)
+#     @test gradtest(Y->pairwise(SqEuclidean(), X, Y), Y)
+#   end
 
-   # Check unary pairwise.
-  @test gradtest(X->pairwise(SqEuclidean(), X), randn(rng, D, P))
-end
+#    # Check unary pairwise.
+#   @test gradtest(X->pairwise(SqEuclidean(), X), randn(rng, D, P))
+# end
 
-function cat_test(f, A::Union{AbstractVector, AbstractMatrix}...)
-  @test gradtest(f, A...)
-  Z, back = Zygote.forward(f, A...)
-  Ā = back(randn(size(Z)))
-  @test all(map((a, ā)->ā isa typeof(a), A, Ā))
-end
+# function cat_test(f, A::Union{AbstractVector, AbstractMatrix}...)
+#   @test gradtest(f, A...)
+#   Z, back = Zygote.forward(f, A...)
+#   Ā = back(randn(size(Z)))
+#   @test all(map((a, ā)->ā isa typeof(a), A, Ā))
+# end
 
-@testset "vcat" begin
+# @testset "vcat" begin
 
-  # Vector-only.
-  cat_test(vcat, randn(1))
-  cat_test(vcat, randn(10))
+#   # Vector-only.
+#   cat_test(vcat, randn(1))
+#   cat_test(vcat, randn(10))
 
-  cat_test(vcat, randn(1), randn(1))
-  cat_test(vcat, randn(5), randn(1))
-  cat_test(vcat, randn(1), randn(6))
-  cat_test(vcat, randn(5), randn(6))
+#   cat_test(vcat, randn(1), randn(1))
+#   cat_test(vcat, randn(5), randn(1))
+#   cat_test(vcat, randn(1), randn(6))
+#   cat_test(vcat, randn(5), randn(6))
 
-  # Matrix-only.
-  for c in [1, 2], r1 in [1, 2], r2 in [1, 2]
-    cat_test(vcat, randn(r1, c))
-    cat_test(vcat, randn(r1, c), randn(r2, c))
-  end
+#   # Matrix-only.
+#   for c in [1, 2], r1 in [1, 2], r2 in [1, 2]
+#     cat_test(vcat, randn(r1, c))
+#     cat_test(vcat, randn(r1, c), randn(r2, c))
+#   end
 
-  # Matrix-Vector / Vector-Matrix / Vector-Matrix-Vector.
-  for r in [1, 2]
-    cat_test(vcat, randn(r, 1), randn(r))
-    cat_test(vcat, randn(r), randn(r, 1))
-    cat_test(vcat, randn(r), randn(r, 1), randn(r))
-  end
-end
+#   # Matrix-Vector / Vector-Matrix / Vector-Matrix-Vector.
+#   for r in [1, 2]
+#     cat_test(vcat, randn(r, 1), randn(r))
+#     cat_test(vcat, randn(r), randn(r, 1))
+#     cat_test(vcat, randn(r), randn(r, 1), randn(r))
+#   end
+# end
 
-@testset "hcat" begin
+# @testset "hcat" begin
 
-  # Vector-only.
-  for r in [1, 2]
-    cat_test(hcat, randn(r))
-    cat_test(hcat, randn(r), randn(r))
-  end
+#   # Vector-only.
+#   for r in [1, 2]
+#     cat_test(hcat, randn(r))
+#     cat_test(hcat, randn(r), randn(r))
+#   end
 
-  # Matrix-only.
-  for r in [1, 2], c1 in [1, 2], c2 in [1, 2]
-    cat_test(hcat, randn(r, c1), randn(r, c2))
-  end
+#   # Matrix-only.
+#   for r in [1, 2], c1 in [1, 2], c2 in [1, 2]
+#     cat_test(hcat, randn(r, c1), randn(r, c2))
+#   end
 
-  # Matrix-Vector / Vector-Matrix / Vector-Matrix-Vector.
-  for r in [1, 2], c in [1, 2]
-    cat_test(hcat, randn(r, c), randn(r))
-    cat_test(hcat, randn(r), randn(r, c))
-    cat_test(hcat, randn(r), randn(r, c), randn(r))
-  end
-end
+#   # Matrix-Vector / Vector-Matrix / Vector-Matrix-Vector.
+#   for r in [1, 2], c in [1, 2]
+#     cat_test(hcat, randn(r, c), randn(r))
+#     cat_test(hcat, randn(r), randn(r, c))
+#     cat_test(hcat, randn(r), randn(r, c), randn(r))
+#   end
+# end
 
-@testset "one(s) and zero(s)" begin
-  @test Zygote.gradient(x->sum(ones(size(x))), randn(5))[1] isa Nothing
-  @test Zygote.gradient(x->sum(one(x)), randn(3, 3))[1] isa Nothing
-  @test Zygote.gradient(x->sum(zeros(size(x))), randn(7))[1] isa Nothing
-  @test Zygote.gradient(x->sum(zero(x)), randn(3))[1] isa Nothing
-end
+# @testset "one(s) and zero(s)" begin
+#   @test Zygote.gradient(x->sum(ones(size(x))), randn(5))[1] isa Nothing
+#   @test Zygote.gradient(x->sum(one(x)), randn(3, 3))[1] isa Nothing
+#   @test Zygote.gradient(x->sum(zeros(size(x))), randn(7))[1] isa Nothing
+#   @test Zygote.gradient(x->sum(zero(x)), randn(3))[1] isa Nothing
+# end
