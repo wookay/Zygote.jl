6543d1159702cfed5cb60ed466e5a1233042a6de
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-04-11 10:53:56 +0000

    build based on 1959bb2

--- dev/search_index.js
+++ dev/search_index.js
@@ -129,11 +129,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "profiling/#Profiling-1",
+    "location": "profiling/#Debugging-in-Time-and-Space-1",
     "page": "Profiling",
-    "title": "Profiling",
+    "title": "Debugging in Time and Space",
     "category": "section",
-    "text": "WIP"
+    "text": "Because Zygote generates Julia code for the backwards pass, many of Julia\'s normal profiling and performance debugging tools work well on it out of the box."
+},
+
+{
+    "location": "profiling/#Performance-Profiling-1",
+    "page": "Profiling",
+    "title": "Performance Profiling",
+    "category": "section",
+    "text": "Julia\'s sampling profiler is useful for understanding performance. We recommend running the profiler in Juno, but the terminal or ProfileView.jl also work well.(Image: )The bars indicate time taken in both the forwards and backwards passes at that line. The canopy chart on the right shows us each function call as a block, arranged so that when f calls g, g gets a block just below f, which is bigger the longer it took to run. If we dig down the call stack we\'ll eventually find the adjoints for things like matmul, which we can click on to view.(Image: )The trace inside the adjoint can be used to distinguish time taken by the forwards and backwards passes."
+},
+
+{
+    "location": "profiling/#Memory-Profiling-1",
+    "page": "Profiling",
+    "title": "Memory Profiling",
+    "category": "section",
+    "text": "Reverse-mode AD typically uses memory proportional to the number of operations in the program, so long-running programs can also suffer memory usage issues. Zygote includes a space profiler to help debug these issues. Like the time profiler, it shows a canopy chart, but this time hovering over it displays the number of bytes stored by each line of the program.(Image: )Note that this currently only works inside Juno."
+},
+
+{
+    "location": "profiling/#Reflection-1",
+    "page": "Profiling",
+    "title": "Reflection",
+    "category": "section",
+    "text": "Julia\'s code and type inference reflection tools can also be useful, though Zygote\'s use of closures can make the output noisy. To see the code Julia runs you should use the low-level _forward method and the pullback it returns. This will directly show either the derived adjoint code or the code for a custom adjoint, if there is one.julia> using Zygote: Context, _forward\n\njulia> add(a, b) = a+b\n\njulia> @code_typed _forward(Context(), add, 1, 2)\nCodeInfo(\n1 ─ %1 = (Base.getfield)(args, 1)::Int64\n│   %2 = (Base.getfield)(args, 2)::Int64\n│   %3 = (Base.add_int)(%1, %2)::Int64\n│   %4 = (Base.tuple)(%3, $(QuoteNode(∂(add))))::PartialTuple(Tuple{Int64,typeof(∂(add))}, Any[Int64, Const(∂(add), false)])\n└──      return %4\n) => Tuple{Int64,typeof(∂(add))}\n\njulia> y, back = _forward(Context(), add, 1, 2)\n(3, ∂(add))\n\njulia> @code_typed back(1)\nCodeInfo(\n1 ─ %1 = (Base.mul_int)(Δ, 1)::Int64\n│   %2 = (Base.mul_int)(Δ, 1)::Int64\n│   %3 = (Zygote.tuple)(nothing, %1, %2)::PartialTuple(Tuple{Nothing,Int64,Int64}, Any[Const(nothing, false), Int64, Int64])\n└──      return %3\n) => Tuple{Nothing,Int64,Int64}"
 },
 
 {
